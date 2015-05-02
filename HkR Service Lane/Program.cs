3138784d85e6f1239d305af9e154e9d2eb5c66fa
jpain <jamesepain@gmail.com>
2015-05-02 13:47:21 +0100

    HkR Service Lane Done

--- HkR Service Lane/Program.cs
+++ HkR Service Lane/Program.cs
@@ -9,10 +9,39 @@ namespace HkR_Service_Lane
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
-            var line1 = Input.GetIntInput("N T (Length of Motorway and Number of Test Cases)", 2);
-            var line2 = Input.GetIntInput("N T (Length of Motorway and Number of Test Cases)", line1[0]);
+            var line1 = Console.ReadLine();
+            var line1Split = line1.Split(' ');
+            var numOfTestCases = int.Parse(line1.Split(' ')[1]);
+
+            var line2 = Console.ReadLine();
+            var laneWidths = line2.Split(' ');
+
+
+            for (var i = 1; i <= numOfTestCases; i++)
+            {
+
+                var input = Console.ReadLine();
+                var inputSplit = input.Split(' ');
+
+                var entrySeg = int.Parse(inputSplit[0]);
+                var exitSeg = int.Parse(inputSplit[1]);
+
+                var minWidth = 3;
+
+                for (var f = entrySeg; f <= exitSeg; f++)
+                {
+                    var laneWidth = int.Parse(laneWidths[f]);
+                    if (laneWidth < minWidth)
+                    {
+                        minWidth = laneWidth;
+                    }
+                }
+
+                Console.WriteLine(minWidth);
+
+            }
 
         }
     }
