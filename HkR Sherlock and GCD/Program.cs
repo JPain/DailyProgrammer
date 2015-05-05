9ec5122c93ef5008738333176326a796a83e31f6
jpain <jamesepain@gmail.com>
2015-05-05 20:04:08 +0100

    HkR Sherlock and GCD In progress

---
+++ HkR Sherlock and GCD/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HkR_Sherlock_and_GCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numOfTestCases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfTestCases; i++)
+            {
+                Console.ReadLine();
+                var array = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+
+            }
+        }
+    }
+}
